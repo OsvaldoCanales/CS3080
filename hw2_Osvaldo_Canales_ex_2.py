4837329c03c4f1c908f03f374c28790ec20c61d1
OsvaldoCanales <osvaldocanales4@gmail.com>
2023-02-14 12:44:55 -0700

    HWK 2 Excercise 2

--- hw2_Osvaldo_Canales_ex_2.py
+++ hw2_Osvaldo_Canales_ex_2.py
@@ -1,8 +1,8 @@
 '''
-Homework 2, Excercise 1 
+Homework 2, Excercise 2
 Osvaldo Canales
 February 14, 2023
-Collatz Sequence program 
+Collatz Sequence program with try and except
 '''
 
 #declare constant
@@ -23,7 +23,16 @@ def collatz(number):
 #Ask the user to enter a digit 
 userNumber = input('Hello! Please enter a digit: ')
 
+
+
 #Loop until the number reaches the COLLATZ_NUMBER
 while userNumber != COLLATZ_NUMBER:
-    userNumber = collatz(int(userNumber))
-    
+
+    #The code will try the collatz sequence 
+    try: 
+        userNumber = collatz(int(userNumber))
+
+    #if user did not enter a whole number then the program will break 
+    except:
+        print('Please use whole numbers only.')
+        break
